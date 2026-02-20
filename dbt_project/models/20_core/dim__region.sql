f4d6289c6901e1a6c470681be1d958fa86e3103a
simon.otte <simon.otte@areto.de>
2026-02-20 22:09:33 +0100

    code linting

--- dbt_project/models/20_core/dim__region.sql
+++ dbt_project/models/20_core/dim__region.sql
@@ -1,6 +1,6 @@
 SELECT
-    regionkey AS region_id,
-    name      AS region_name,
-    comment   AS region_comment,
+    regionkey   AS region_id,
+    name        AS region_name,
+    comment     AS region_comment,
     created_at
 FROM {{ ref('stg__region') }}
