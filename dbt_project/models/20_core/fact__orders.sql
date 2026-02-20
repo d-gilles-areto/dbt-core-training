14b11b2a9fda607fcc992f1f895f0b2a9c30a06e
simon.otte <simon.otte@areto.de>
2026-02-20 21:17:58 +0100

    Aufräumen - Endstand ex_03

--- dbt_project/models/20_core/fact__orders.sql
+++ dbt_project/models/20_core/fact__orders.sql
@@ -1,8 +1,8 @@
 SELECT
-    orderkey,
-    custkey,
-    orderdate,
-    orderstatus,
-    orderpriority,
+    orderkey AS order_id,
+    custkey AS customer_id,
+    orderdate AS order_date,
+    orderstatus AS order_status,
+    orderpriority AS order_priority,
     created_at
 FROM {{ ref('stg__orders') }}
