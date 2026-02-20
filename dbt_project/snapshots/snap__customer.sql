8adffe2b6a76b24af854e511f10f605845f35d31
simon.otte <simon.otte@areto.de>
2026-02-20 09:32:24 +0100

    Organisation im dbt_project.yml

--- dbt_project/snapshots/snap__customer.sql
+++ dbt_project/snapshots/snap__customer.sql
@@ -1,7 +1,6 @@
 {% snapshot snap__customer %}
 
 {{ config(
-    target_schema = generate_schema_name('snapshots'),
     unique_key = 'c_custkey',
     strategy = 'check',
     check_cols = ['c_address', 'c_phone', 'c_acctbal']
