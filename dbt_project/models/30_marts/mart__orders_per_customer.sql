02ffadfbcd4b7db87fc903a6b05d75758c7d19ba
simon.otte <simon.otte@areto.de>
2026-02-20 22:08:33 +0100

    code linting

--- dbt_project/models/30_marts/mart__orders_per_customer.sql
+++ dbt_project/models/30_marts/mart__orders_per_customer.sql
@@ -12,15 +12,12 @@ SELECT
     customer.market_segment,
     EXTRACT(YEAR FROM orders.order_date) AS order_year,
     COUNT(*) AS total_orders
-
 FROM orders
-LEFT JOIN customer ON orders.customer_id = customer.customer_id
-
+    LEFT JOIN customer ON orders.customer_id = customer.customer_id
 GROUP BY
     customer.customer_id,
     customer.customer_name,
     customer.market_segment,
     EXTRACT(YEAR FROM orders.order_date)
-
 ORDER BY
     customer.customer_id
