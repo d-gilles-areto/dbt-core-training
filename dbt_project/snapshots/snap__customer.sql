176d18dc625c77f300c4f7b53e3f1fc3d0bbd4cd
David Gilles <info@davidgilles.de>
2026-02-20 16:44:53 +0100

    feat: centralize snapshot configuration in macro (AP6)

--- dbt_project/snapshots/snap__customer.sql
+++ dbt_project/snapshots/snap__customer.sql
@@ -1,11 +1,17 @@
 {% snapshot snap__customer %}
 
-{{ config(
-    unique_key = 'c_custkey',
-    strategy = 'check',
-    check_cols = ['c_address', 'c_phone', 'c_acctbal']
-) }}
+{%- set config = get_snapshot_config('snap__customer') -%}
 
-SELECT * FROM {{ source('tpch', 'customer_tpch') }}
+{{ render_snapshot_config(config) }}
 
-{% endsnapshot %}
+SELECT
+    custkey,
+    name,
+    address,
+    phone,
+    acctbal,
+    mktsegment,
+    comment,
+    nationkey,
+    created_at
+FROM {{ source('tpch', 'customer_tpch') }}
