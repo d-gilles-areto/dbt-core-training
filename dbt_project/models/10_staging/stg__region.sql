9a9db114bdf205286d3fdd2dd01ce51cf11ec5d8
simon.otte <simon.otte@areto.de>
2026-02-20 22:06:47 +0100

    code linting

--- dbt_project/models/10_staging/stg__region.sql
+++ dbt_project/models/10_staging/stg__region.sql
@@ -1,6 +1,6 @@
 SELECT
     r_regionkey AS regionkey,
-    r_name AS name,
-    r_comment AS comment,
+    r_name      AS name,
+    r_comment   AS comment,
     created_at
 FROM {{ source('tpch', 'region_tpch') }}
--- dbt_project/models/20_core/fact__orders.sql
+++ dbt_project/models/20_core/fact__orders.sql
@@ -1,8 +1,8 @@
 SELECT
-    orderkey AS order_id,
-    custkey AS customer_id,
-    orderdate AS order_date,
-    orderstatus AS order_status,
-    orderpriority AS order_priority,
+    orderkey        AS order_id,
+    custkey         AS customer_id,
+    orderdate       AS order_date,
+    orderstatus     AS order_status,
+    orderpriority   AS order_priority,
     created_at
 FROM {{ ref('stg__orders') }}
