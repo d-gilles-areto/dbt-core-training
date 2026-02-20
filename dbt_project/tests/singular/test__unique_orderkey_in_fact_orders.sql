277c29df53bc192e58ea530bba34256e98e14e56
David Gilles <info@davidgilles.de>
2026-02-20 16:38:34 +0100

    ex_09: remove singular tests (incremental models exercise)

--- dbt_project/tests/singular/test__unique_orderkey_in_fact_orders.sql
+++
@@ -1,9 +0,0 @@
--- Dieser Test prüft, ob jeder order_id in fact__orders nur einmal vorkommt.
--- Wenn Rows zurückkommen, gibt es Duplikate.
-
-SELECT
-    order_id,
-    COUNT(*) AS row_count
-FROM {{ ref('fact__orders') }}
-    GROUP BY order_id
-        HAVING COUNT(*) > 1
