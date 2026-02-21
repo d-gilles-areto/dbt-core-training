aa689b6c00c358d668eb8bf67b1ac9523d621dda
simon.otte <simon.otte@areto.de>
2026-02-21 22:45:29 +0100

    Aufräumen - Endstand ex_12

--- dbt_project/models/20_core/dim__customer.sql
+++ dbt_project/models/20_core/dim__customer.sql
@@ -1,16 +1,11 @@
-WITH customer AS (
-    SELECT *
-    FROM {{ ref('stg__customer') }}
-)
-
 SELECT
-    customer.custkey,
-    customer.name,
-    customer.address,
-    customer.phone,
-    customer.acctbal,
-    customer.mktsegment,
-    customer.comment,
-    customer.nationkey,
-    customer.created_at
-FROM customer
+    custkey     AS customer_id,
+    name        AS customer_name,
+    address,
+    phone,
+    acctbal     AS account_balance,
+    mktsegment  AS market_segment,
+    comment     AS customer_comment,
+    nationkey   AS nation_id,
+    created_at
+FROM {{ ref('stg__customer') }}
