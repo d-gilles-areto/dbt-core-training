fa944605702e8a8792a0b8d622ff1667aba824f9
simon.otte <simon.otte@areto.de>
2026-02-21 22:07:13 +0100

    Aufräumen - Endstand ex_09

--- dbt_project/models/20_core/fact__order_lines.sql
+++ dbt_project/models/20_core/fact__order_lines.sql
@@ -7,40 +7,37 @@ WITH orders AS (
     SELECT * FROM {{ ref('stg__orders') }}
 ),
 
-lines AS (
+lineitem AS (
     SELECT * FROM {{ ref('stg__lineitem') }}
 )
 
 SELECT
-    lines.orderkey,
-    lines.linenumber,
-    orders.custkey,
-    orders.orderdate,
-    lines.shipdate,
-    lines.commitdate,
-    lines.receiptdate,
-    orders.orderstatus,
-    orders.orderpriority,
-    lines.returnflag,
-    lines.linestatus,
-    lines.shipmode,
-    lines.quantity,
-    lines.extendedprice,
-    lines.discount,
-    lines.tax,
-    lines.extendedprice * (1 - lines.discount) AS net_amount,
-    lines.extendedprice * lines.discount AS discount_amount,
-    (lines.extendedprice * (1 - lines.discount)) * (1 + lines.tax) AS net_amount_incl_tax,
+    lineitem.orderkey       AS order_id,
+    lineitem.linenumber     AS line_number,
+    orders.custkey          AS customer_id,
+    orders.orderdate        AS order_date,
+    lineitem.shipdate       AS ship_date,
+    lineitem.commitdate     AS commit_date,
+    lineitem.receiptdate    AS receipt_date,
+    orders.orderstatus      AS order_status,
+    orders.orderpriority    AS order_priority,
+    lineitem.returnflag     AS return_flag,
+    lineitem.linestatus     AS line_status,
+    lineitem.shipmode       AS ship_mode,
+    lineitem.quantity,
+    lineitem.extendedprice  AS extended_price,
+    lineitem.discount,
+    lineitem.tax,
+    lineitem.extendedprice * (1 - lineitem.discount)                        AS net_amount,
+    lineitem.extendedprice * lineitem.discount                              AS discount_amount,
+    (lineitem.extendedprice * (1 - lineitem.discount)) * (1 + lineitem.tax) AS net_amount_incl_tax,
     CASE
-        WHEN lines.shipdate > lines.commitdate THEN true
+        WHEN lineitem.shipdate > lineitem.commitdate 
+            THEN true
         ELSE false
     END AS is_late_shipment,
-    (lines.shipdate - orders.orderdate) AS days_to_ship,
-    (lines.receiptdate - orders.orderdate) AS days_to_receive,
-    lines.created_at
-FROM lines
-    LEFT JOIN orders ON lines.orderkey = orders.orderkey
-
-{% if is_incremental() %}
-    WHERE lines.created_at > (SELECT MAX(created_at) FROM {{ this }})
-{% endif %}
+    (lineitem.shipdate - orders.orderdate)      AS days_to_ship,
+    (lineitem.receiptdate - orders.orderdate)   AS days_to_receive,
+    lineitem.created_at
+FROM lineitem
+    LEFT JOIN orders ON lineitem.orderkey = orders.orderkey
--- dbt_project/models/30_marts/mart__customer_per_country.sql
+++ dbt_project/models/30_marts/mart__customer_per_country.sql
@@ -1,7 +1,9 @@
 SELECT
     geo.nation,
     geo.region,
-    COUNT(*)        AS customer_count
-
+    COUNT(*) AS customer_count
 FROM {{ ref('dim__customer') }} AS customer
-LEFT JOIN {{ ref('dim__geo') }} AS geo ON customer.nationkey = geo.nationkey
+    LEFT JOIN {{ ref('dim__geo') }} AS geo ON customer.nation_id = geo.nation_id
+GROUP BY 
+    geo.nation,
+    geo.region
