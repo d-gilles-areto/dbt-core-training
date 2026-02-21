6df6bdd35c1ac533da7335db1d1806c4ad888861
simon.otte <simon.otte@areto.de>
2026-02-21 23:20:21 +0100

    Anpassung snapshot

--- dbt_project/snapshots/snap__customer.sql
+++ dbt_project/snapshots/snap__customer.sql
@@ -1,13 +1,23 @@
 {% snapshot snap__customer %}
 
+{{
+  config(
+    unique_key = 'c_custkey',
+    strategy = 'timestamp',
+    updated_at = 'created_at'
+  )
+}}
+
 SELECT
-    custkey,
-    name,
-    address,
-    phone,
-    acctbal,
-    mktsegment,
-    comment,
-    nationkey,
+    c_custkey,
+    c_name,
+    c_address,
+    c_phone,
+    c_acctbal,
+    c_mktsegment,
+    c_comment,
+    c_nationkey,
     created_at
 FROM {{ source('tpch', 'customer_tpch') }}
+
+{% endsnapshot %}
