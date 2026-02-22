5b0c6bbfc2d645cc3af1f41cc7d599f7f791c3a3
David Gilles <info@davidgilles.de>
2026-02-22 19:38:18 +0100

    fix: A1 fact__orders.yml column names, A2 remove phantom cols, A6 replace mart (with semicolon bug)

---
+++ dbt_project/models/30_marts/mart__order_summary.sql
@@ -0,0 +1,27 @@
+WITH orders AS (
+    SELECT * FROM {{ ref('fact__orders') }}
+),
+
+customer AS (
+    SELECT * FROM {{ ref('dim__customer') }}
+),
+
+geo AS (
+    SELECT * FROM {{ ref('dim__geo') }}
+)
+
+SELECT
+    orders.order_id,
+    customer.customer_name,
+    customer.market_segment,
+    geo.nation,
+    geo.region,
+    orders.order_date,
+    orders.order_status,
+    orders.line_count,
+    orders.total_net_amount,
+    orders.has_late_shipment
+FROM orders
+    LEFT JOIN customer ON orders.customer_id = customer.customer_id
+    LEFT JOIN geo ON customer.nation_id = geo.nation_id
+ORDER BY orders.order_date DESC;
