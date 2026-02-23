5f50131f39039d47eec8bba80077c25ae167e403
David Gilles <info@davidgilles.de>
2026-02-23 17:59:31 +0100

    fix: remove order_year from mart__orders_per_customer

--- dbt_project/models/30_marts/mart__orders_per_customer.sql
+++ dbt_project/models/30_marts/mart__orders_per_customer.sql
@@ -10,14 +10,12 @@ SELECT
     customer.customer_id,
     customer.customer_name,
     customer.market_segment,
-    EXTRACT(YEAR FROM orders.order_date) AS order_year,
     COUNT(*) AS total_orders
 FROM orders
     LEFT JOIN customer ON orders.customer_id = customer.customer_id
 GROUP BY
     customer.customer_id,
     customer.customer_name,
-    customer.market_segment,
-    EXTRACT(YEAR FROM orders.order_date)
+    customer.market_segment
 ORDER BY
     customer.customer_id
