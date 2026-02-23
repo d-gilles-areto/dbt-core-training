d4b9e76baa96245acd1444e7e0396c148c08d79a
David Gilles <info@davidgilles.de>
2026-02-23 18:34:49 +0100

    chore: add explicit CAST expressions to staging models (from final_state)

--- dbt_project/models/10_staging/stg__customer.sql
+++ dbt_project/models/10_staging/stg__customer.sql
@@ -1,11 +1,11 @@
 SELECT
-    c_custkey       AS custkey,
-    c_mktsegment    AS mktsegment,
-    c_nationkey     AS nationkey,
-    c_name          AS name,
-    c_address       AS address,
-    c_phone         AS phone,
-    c_acctbal       AS acctbal,
-    c_comment       AS comment,
-    created_at
-FROM {{ source('tpch', 'customer_tpch') }}
+    CAST(c_custkey    AS INTEGER)   AS custkey,
+    CAST(c_mktsegment AS TEXT)      AS mktsegment,
+    CAST(c_nationkey  AS INTEGER)   AS nationkey,
+    CAST(c_name       AS TEXT)      AS name,
+    CAST(c_address    AS TEXT)      AS address,
+    CAST(c_phone      AS TEXT)      AS phone,
+    CAST(c_acctbal    AS NUMERIC)   AS acctbal,
+    CAST(c_comment    AS TEXT)      AS comment,
+    CAST(created_at   AS TIMESTAMP) AS created_at
+FROM {{ ref('snap__customer') }} -- Nachdem die Quelle für Customers über den Snapshot (./snapshots/snap__customer.sql) gem. SCD2 historisiert wurde, wollen wir nun die historisierte Quelle in der weiteren Pipeline benutzen.
